a55107839d4af6d23ffcecd443999539aeca3c2d
Mirko <tartaruga88@gmail.com>
2019-11-25 22:08:05 +0100

    code refactor

--- MqttCommunicationTest/Program.cs
+++ MqttCommunicationTest/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Text;
-using System.Threading.Tasks;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
 
@@ -14,28 +13,9 @@ namespace MqttCommunicationTest
         {
             string message = "Test Message";
 
-            //Task.Run(() => 
-            //{
-            //    try
-            //    {
-            //        var config = new MqttConfiguration { Port = 1883 };
-            //        var client = MqttClient.CreateAsync("test.mosquitto.org", config).Result;
-            //        var clientId = "myClientID";
-
-            //        client.ConnectAsync(new MqttClientCredentials(clientId)).Wait();
-            //        client.SubscribeAsync(topic, MqttQualityOfService.AtLeastOnce).Wait();
-            //        //Publishes "message" Var 
-            //        client.PublishAsync(new MqttApplicationMessage(topic, Encoding.UTF8.GetBytes(message)), MqttQualityOfService.AtLeastOnce).Wait();
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        Console.WriteLine(ex.Message.ToString());
-            //    }
-            //});
-
             Console.WriteLine("Hello, World!");
             // Create Client instance
-            uPLibrary.Networking.M2Mqtt.MqttClient myClient = new uPLibrary.Networking.M2Mqtt.MqttClient("test.mosquitto.org", 1883, false, null, null, MqttSslProtocols.None, null);
+            MqttClient myClient = new MqttClient("127.0.0.1", 1883, false, null, null, MqttSslProtocols.None, null);
 
             // Register to message received
             myClient.MqttMsgPublishReceived += client_recievedMessage;
@@ -46,6 +26,8 @@ namespace MqttCommunicationTest
             // Subscribe to topic
             myClient.Subscribe(new string[] { "/testing" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
 
+            myClient.Publish("/testing", Encoding.ASCII.GetBytes(message));
+
             Console.ReadLine();
         }
 
@@ -54,26 +36,6 @@ namespace MqttCommunicationTest
             // Handle message received
             var message = Encoding.Default.GetString(e.Message);
             Console.WriteLine("Message received: " + message);
-        }
-
-        public async Task MqttConnectionTest(string message)
-        {
-            //...
-            //try
-            //{
-            //    var config = new MqttConfiguration { Port = 1883 };
-            //    var client = MqttClient.CreateAsync("test.mosquitto.org", config).Result;
-            //    var clientId = "myClientID";
-
-            //    client.ConnectAsync(new MqttClientCredentials(clientId)).Wait();
-            //    client.SubscribeAsync(topic, MqttQualityOfService.AtLeastOnce).Wait();
-            //    //Publishes "message" Var 
-            //    client.PublishAsync(new MqttApplicationMessage(topic, Encoding.UTF8.GetBytes(message)), MqttQualityOfService.AtLeastOnce).Wait();
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine(ex.Message.ToString());
-            //}
         }
     }
 }
